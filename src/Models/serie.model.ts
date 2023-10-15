1c030c4c077e71374b8eab6108a388db3092c163
Wallys Ferreira <wallysfsilva@gmail.com>
2023-10-15 19:29:43 +0000

    use validation annotations

--- src/Models/serie.model.ts
+++ src/Models/serie.model.ts
@@ -1,6 +1,16 @@
+import { IsNotEmpty, IsString, IsInt } from 'class-validator';
+
 export class Serie {
+    @IsNotEmpty()
+    @IsString()
     name: string;
+
+    @IsString()
     platform: string;
+
+    @IsInt()
     release_year: number;
+
+    @IsInt()
     number_seasons: number;
 }

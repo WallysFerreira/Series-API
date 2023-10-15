ee76d6af915859ede3dbc549372d48b2ba5b48af
Wallys Ferreira <wallysfsilva@gmail.com>
2023-10-15 18:58:55 +0000

    create endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Post()
+  create(): string {
+    return this.appService.create();
+  }
+
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getAll(): string {
+    return this.appService.getAll();
+  }
+
+  @Delete()
+  delete(): string {
+    return this.appService.delete()
+  }
+
+  @Put()
+  update(): string {
+    return this.appService.update();
   }
 }

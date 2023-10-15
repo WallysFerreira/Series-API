5fdf48a0a558752f4ab3dfb7da876bf21bc9af4a
Wallys Ferreira <wallysfsilva@gmail.com>
2023-10-15 19:03:00 +0000

    create placeholder service functions

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,19 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  create(): string {
+    return 'WIP';
+  }
+  
+  getAll(): string {
+    return 'WIP';
+  }
+
+  delete(): string {
+    return 'WIP';
+  }
+
+  update(): string {
+    return 'WIP';
   }
 }

88433936a89b45a7de9f3dde5eac29003c4a99ce
Wallys Ferreira <wallysfsilva@gmail.com>
2023-10-15 19:52:46 +0000

    finish create function

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { Serie } from './Models/serie.model';
+import 'dotenv/config';
+
+const { MongoClient } = require('mongodb');
+const dbName = "series";
+const client = new MongoClient(process.env.MONGO_URI);
 
 @Injectable()
 export class AppService {
-  create(): string {
-    return 'WIP';
+  async create(serie: Serie): Promise<string> {
+    try {
+      await client.connect();
+      const collection = client.db(dbName).collection(dbName);
+
+      const result = await collection.insertMany([serie]);
+
+      return `Inserted document with id ${result.insertedIds['0']}`;
+    } catch (err) {
+      return err;
+    } finally {
+
+    }
   }
   
   getAll(): string {
